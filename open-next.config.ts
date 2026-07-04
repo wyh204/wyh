6ee43f6342fa989afabf4d320d068eec4cadb2f7
GLUT Student <glut-student@example.com>
2026-07-04 23:53:00 +0800

    feat: add Cloudflare deployment config (wrangler.toml, open-next.config.ts)

---
+++ open-next.config.ts
@@ -0,0 +1,5 @@
+// OpenNext Cloudflare configuration
+// See: https://opennext.js.org/cloudflare/get-started
+import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
+
+export default defineCloudflareConfig({});
