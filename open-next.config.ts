80344c4945089e741d8f7239071eecb90dff3ad8
GLUT Student <glut-student@example.com>
2026-07-06 21:37:48 +0800

    fix: remove invalid minify option

--- open-next.config.ts
+++ open-next.config.ts
@@ -2,8 +2,4 @@
 // See: https://opennext.js.org/cloudflare/get-started
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
 
-export default defineCloudflareConfig({
-  // Bundle all server chunks into the worker to avoid ChunkLoadError
-  // This prevents runtime chunk loading failures on Cloudflare Workers
-  minify: true,
-});
+export default defineCloudflareConfig({});
